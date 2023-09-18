29fb7c51ff68c22366692f18d02eb0e57db9a77e
Isaac Ghorzi <ghorzi.isaac@gmail.com>
2023-09-18 15:04:58 +0200

    update

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,14 +2,75 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
+use App\Entity\Artiste;
+use App\Entity\Album;
+use App\Entity\Morceau;
+use App\Entity\Style;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
-{
+{    
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create("fr_FR");
+        
+        function fload(?String $file): array
+        {
+            $f = fopen(__DIR__."/".$file,'r');
+            while (!feof($f)) {
+                $resultat[] = fgetcsv($f);
+            }
+            fclose($f);
+            return $resultat;
+        }
+        
+        // Artistes
+        $lesArtistes = fload("artiste.csv");
+        $genre = ["man", "woman"];
+        foreach ($lesArtistes as $var) {
+            $artiste = new Artiste();
+            $artiste    -> setNom($var[1])
+                        -> setDescription("<p>partez du principe que c'est un long paragraphe</p><p>cette ligne de caractères est aussi un paragraphe</p>")
+                        -> setSite("cestuneurl.com")
+                        -> setImage("https://randomuser.me/api/portraits/".$genre[mt_rand(0,1)]."/".mt_rand(1,99).".jpg")
+                        -> setType($var[2]);
+            $manager->persist($artiste);
+            $this->addReference("artiste".$artiste->getId(), $artiste);
+        }
+
+        // Styles
+        $lesStyles = fload("style.csv");
+        foreach ($lesStyles as $vst) {
+            $style = new Style();
+            $style      ->setLibelle($vst[1])
+                        ->setCouleur($faker->safeHexColor());
+            $manager->persist($style);
+            $this.addReference("style".$style->getId(), $style);
+        }
+
+        // Albums
+        $lesAlbums = fload("album.csv");
+        foreach ($lesAlbums as $val) {
+            $album = new Album();
+            $album      -> setNom($val[1])
+                        -> setDate(intval($val[2]))
+                        -> setArtiste($this->getReference("artiste".$val[4]));
+            $manager->persist($album);
+            $this.addReference("album".$album->getId(), $album);
+        }
+
+        //morceau
+        $lesMorceau = fload("morceau.csv");
+        foreach ($lesMorceau as $vmo) {
+            $morceau = new Morceau();
+            $morceau    -> setTitre($vmo[2])
+                        -> setDuree(date("i:s",$vmo[3]))
+                        -> setArtiste($this->getReference("artiste".$vmo[4]));
+            $manager->persist($morceau);
+            $this.addReference("morceau".$morceau->getId(), $morceau);
+        }
 
         $manager->flush();
     }
---
+++ src/Entity/Style.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StyleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StyleRepository::class)]
+class Style
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $libelle = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $couleur = null;
+
+    #[ORM\ManyToMany(targetEntity: Album::class, inversedBy: 'styles')]
+    private Collection $albums;
+
+    public function __construct()
+    {
+        $this->albums = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): static
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getCouleur(): ?string
+    {
+        return $this->couleur;
+    }
+
+    public function setCouleur(string $couleur): static
+    {
+        $this->couleur = $couleur;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Album>
+     */
+    public function getAlbums(): Collection
+    {
+        return $this->albums;
+    }
+
+    public function addAlbum(Album $album): static
+    {
+        if (!$this->albums->contains($album)) {
+            $this->albums->add($album);
+        }
+
+        return $this;
+    }
+
+    public function removeAlbum(Album $album): static
+    {
+        $this->albums->removeElement($album);
+
+        return $this;
+    }
+}
